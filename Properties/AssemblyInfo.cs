7048bb7c42df28c0a11dfd67806adefce220c33c
me <me@me.com>
2017-10-24 22:31:38 +0100

    first commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("InTime")]
+[assembly: AssemblyDescription("Make Music With Dance")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: CLSCompliant(true)]

90c2e62fab9da8e44649751bb96425fe162ece77
ptruscott <perantruscott@gmail.com>
2017-11-02 00:31:48 +0000

    added peak class

---
+++ Peak.cs
@@ -0,0 +1,29 @@
+﻿namespace InTime
+{
+    class Peak
+    {
+        private float size;
+        private float timeStamp;
+
+        public Peak(float size, float timeStamp) {
+            this.size = size;
+            this.timeStamp = timeStamp;
+        }
+
+        public float getSize() {
+            return size;
+        }
+
+        public float getTimeStampe() {
+            return timeStamp;
+        }
+
+        public void timeStep() {
+            timeStamp--;
+        }
+
+        public void setSize(float size) {
+            this.size = size;
+        }
+    }
+}
